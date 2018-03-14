154c66e4715da5d59fadf583a4c750bab44aabcb
mayankroy <mayank.ry@gmail.com>
2018-03-14 00:53:33 -0400

    minichanges

--- HW4/13/RHS.m
+++ HW4/13/RHS.m
@@ -6,7 +6,7 @@ global sat
 R = [z(1:3)';z(4:6)';z(7:9)'];
 w = z(10:12);
 I = R * (sat.I * R');
-%I = sat.I ;
+I = sat.I ;
 ohm = [0     -w(3)   w(2);
        w(3)     0   -w(1);
       -w(2)   w(1)      0];
